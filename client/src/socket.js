70ade0c22931bdf189ea90e1b659dcc138bc8165
Anuraj Singh <juluanurajsingh@gmail.com>
2025-06-26 16:43:44 +0530

    added firebase auth email/password and loginWithGoogle

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,21 @@
+// src/socket.js
 import { io } from 'socket.io-client';
+import { auth } from './firebase';
+import { onAuthStateChanged } from 'firebase/auth';
 
-const socket = io('http://localhost:5000'); // or your backend URL
+const socket = io('http://localhost:5000', {
+  autoConnect: false,
+});
+
+// Listen for Firebase login
+onAuthStateChanged(auth, async (user) => {
+  if (user) {
+    const token = await user.getIdToken();
+    socket.auth = { token };
+    socket.connect();
+  } else {
+    console.warn("User not logged in, socket not connected.");
+  }
+});
 
 export default socket;
