6699de13235b7feb37ce40125cc17772ec46220f
its-me-navee <navee4501@gmail.com>
2025-07-12 03:28:55 +0530

    change in workflow

--- server/server.js
+++ server/server.js
@@ -18,10 +18,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, 'client-build')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client-build', 'index.html'));
 });
 
 const server = http.createServer(app);
