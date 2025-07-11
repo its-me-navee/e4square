d8e93106a563c89cd192f96b5b554aed176a7b4c
its-me-navee <navee4501@gmail.com>
2025-07-11 19:44:00 +0530

    changed github workflow and server.js for host ready site

--- server/server.js
+++ server/server.js
@@ -18,6 +18,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {origin: "*"}
