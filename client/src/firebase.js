39c0e70515956da53189951020ebdaf8da88d7c6
idris-1729 <mdidrissameer@gmail.com>
2025-07-15 19:31:46 +0530

    frontend exposed

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyDd9qGHxScGSlyfOOYcyeNPW8p2OebtuzU",
+  authDomain: "e4square-5ed72.firebaseapp.com",
+  projectId: "e4square-5ed72",
+  storageBucket: "e4square-5ed72.appspot.com",
+  messagingSenderId: "539074674710",
+  appId: "1:539074674710:web:1dae89772de2111f6bf537"
 };
 
 const app = initializeApp(firebaseConfig);
