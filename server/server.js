318f77afaea7196dc704bfc8659b2cea07fe66fc
ANURAJ SINGH <juluanurajsingh@gmail.com>
2025-07-30 16:29:09 +0530

    removed require-corp

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 
 app.use((req, res, next) => {
   res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-  res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+  // res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
   next();
 });
 
